fa087cff3f392767f90b4bcc5003d993d99ff2d3
dgsyrc <1779252249@qq.com>
2019-06-24 09:45:03 +0800

    Site updated: 2019-06-24 09:44:57

---
+++ live/autoload.js
@@ -0,0 +1,28 @@
+const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/";
+
+$("<link>").attr({href: /live/ + "waifu.css", rel: "stylesheet", type: "text/css"}).appendTo("head");
+//waifu.css的绝对路径
+
+$.ajax({
+	url: /live/ + "live2d.min.js",
+	dataType: "script",
+	cache: true,
+	async: false
+});
+//live2d.min.js的绝对路径
+
+$.ajax({
+	url: /live/ + "waifu-tips.js",
+	dataType: "script",
+	cache: true,
+	async: false
+});
+//waifu-tips.js的绝对路径
+
+//初始化看板娘，会自动加载指定目录下的 waifu-tips.json
+$(window).on("load", function() {
+	initWidget(/live/ + "waifu-tips.json", "https://live2d.fghrsh.net/api");
+});
+//initWidget第一个参数为waifu-tips.json的绝对路径
+//第二个参数为api地址（无需修改）
+//api后端可自行搭建，参考https://github.com/fghrsh/live2d_api
